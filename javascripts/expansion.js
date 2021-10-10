995e1993a0e37890642325330e01054d3f0908f9
SebastianH2000 <87954095+SebastianH2000@users.noreply.github.com>
2021-10-09 23:49:27 -0400

    Delete javascripts directory

--- javascripts/expansion.js
+++
@@ -1,6 +0,0 @@
-function gainExpansions() {
-    if (player.energy.exponent > player.expansions) {
-        player.expansions = player.expansions.add(player.energy.exponent - player.expansions);
-        player.energy = new Decimal("0");
-    }
-}
